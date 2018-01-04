4f384105783777fa7eebc705d56dce78291a7a0e
Sebastian Baum <sebastian.baum@ion2s.com>
2018-01-04 15:20:44 +0100

    - initial commit

---
+++ src/TosServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sebbaum\Tos;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class TosServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
