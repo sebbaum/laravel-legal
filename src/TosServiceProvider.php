9f195c852503438d5b09a1ab3575129b5c8c61e7
Sebastian Baum <sebastian.baum@ion2s.com>
2018-01-16 08:36:38 +0100

    - config management integrated

--- src/TosServiceProvider.php
+++ src/TosServiceProvider.php
@@ -8,11 +8,15 @@ class TosServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
-        //
+        $this->configure();
+
+        $this->publishes([
+            __DIR__ . '/../config/tos.php' => config_path('tos.php')
+        ], 'tos-config');
     }
 
-    public function boot()
+    private function configure()
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../config/tos.php', 'tos');
     }
 }
