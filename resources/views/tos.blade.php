6dba201720d43aa3bc4dfe0b563f9bc8da77fa5f
Sebastian Baum <sebastian.baum@ion2s.com>
2018-01-19 14:51:23 +0100

    - integration of routes - integration and publishing of views - removed obsolete files

---
+++ resources/views/tos.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Terms Of Service</title>
+
+  </head>
+
+  <body>
+    <h1>Terms Of Service</h1>
+
+  </body>
+</html>
--- src/TosServiceProvider.php
+++ src/TosServiceProvider.php
@@ -2,21 +2,71 @@
 
 namespace Sebbaum\Tos;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class TosServiceProvider extends BaseServiceProvider
 {
+
+    /**
+     * Boot the Package.
+     */
+    public function boot()
+    {
+        $this->addRoutes();
+        $this->addResources();
+    }
+
+    /**
+     * Add routes.
+     */
+    private function addRoutes()
+    {
+        Route::group([
+            'prefix' => config('tos.uri', 'tos'),
+            'namespace' => 'Sebbaum\Tos\Http\Controllers',
+            'middleware' => 'web'
+        ], function () {
+            $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        });
+    }
+
+    /**
+     * Add the views for this package.
+     */
+    private function addResources()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'tos');
+    }
+
+    /**
+     * Do registration stuff.
+     */
     public function register()
     {
         $this->configure();
-
-        $this->publishes([
-            __DIR__ . '/../config/tos.php' => config_path('tos.php')
-        ], 'tos-config');
+        $this->publishFiles();
     }
 
+    /**
+     * Merge configurations.
+     */
     private function configure()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/tos.php', 'tos');
+    }
+
+    /**
+     * Publish package's configs and views.
+     */
+    private function publishFiles()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/tos.php' => config_path('tos.php')
+        ], 'tos-config');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/tos'),
+        ], 'tos-views');
     }
 }
