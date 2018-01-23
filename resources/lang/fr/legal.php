71a7d002128d0335a60abb06b07a3bc08fd04df2
Sebastian Baum <sebastian.baum@ion2s.com>
2018-01-23 08:24:07 +0100

    - added french translations

---
+++ resources/lang/fr/legal.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'tos' => 'Conditions générales',
+    'tos_short' => 'Conditions générales',
+    'pripol' => 'Déclaration de protection des données',
+    'imprint' => 'Mentions obligatoires'
+];
