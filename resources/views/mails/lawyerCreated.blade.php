8e6bbe5a27107ff9a9a6056bdc9906f44dfd9d01
Sebastian Baum <sebastian.baum@ion2s.com>
2018-02-12 11:18:59 +0100

    - better formatting for the mail template

--- resources/views/mails/lawyerCreated.blade.php
+++ resources/views/mails/lawyerCreated.blade.php
@@ -2,15 +2,18 @@
 
 # Dear {{ $lawyer->title }} {{ $lawyer->firstname }} {{ $lawyer->surname }}
 
-You have been granted access to edit legal pages of {{ config('app.name') }}.
-Your login credentials are as follows:
-Username: {{ $lawyer->email }}
-Password: {{ $password }}
+You have been granted access to edit legal pages of {{ config('app.name') }}.<br>
+Your login credentials are as follows:<br>
+
+<p>
+    Username: {{ $lawyer->email }}<br>
+    Password: {{ $password }}
+</p>
 
 @component('mail::button', ['url' => url('/' . config('legal.uri') . '/editor')])
 Login
 @endcomponent
 
-Thank you for contributing to our application
+Thank you for contributing to our application.
 
 @endcomponent
