a6d8903c3d6e192b7cb4bb241763bd3c188b2eb5
Sebastian Baum <sebastian.baum@ion2s.com>
2018-02-16 14:57:14 +0100

    - prepared command to update a Lawyer

---
+++ src/Console/Commands/UpdateLawyer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sebbaum\Legal\Console;
+
+use Illuminate\Console\Command;
+
+class UpdateLawyer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'legal:update-lawyer';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update a Lawyer';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
